a242a72ec38c63d87120325314165af1c2035ddd
liza31337 <liza31337@9db8dcc5-964d-0410-8538-7deeb381aa69>
2008-05-19 23:16:47 +0000

    Added xslt

--- tei2epub/bin/settings.py
+++ tei2epub/bin/settings.py
@@ -1,13 +1,15 @@
 from os.path import realpath, dirname
 import sys, logging
 
+# Raise or lower this setting according to the amount of debugging
+# output you would like to see.  See http://docs.python.org/lib/module-logging.html
 logging.basicConfig(level=logging.WARN)
+
+# Run epubcheck after each build? (Recommended)
+VALIDATE = True
 
 # Path of the executable
 path = realpath(dirname(sys.argv[0]))
-
-# TEI is not included in the distribution
-TEI2XHTML_XSLT = '%s/../../xsl/tei-xsl-5.9/p5/xhtml/tei.xsl' % path
 
 # XSL templates
 XSLT_DIR = '%s/../xsl' % path
@@ -22,8 +24,27 @@ DIST = '%s/../dist' % path
 BUILD = '%s/../build' % path
 
 # zip command
-ZIP = 'zip'
+ZIP = '/usr/bin/zip'
 
+# Arguments to pass to zip (this will specify the order of the files and not to 
+# compress the mimetype file
+ZIP_ARGS = '-qXr9D'
+
+# Configuration specific to your TEI type
+TEI_DIV_TYPE = 'chapter'
+
+# TEI P5 version 5.9 is included in the distribution but could be replaced
+# with a different version (e.g. p4)
+TEI2XHTML_XSLT = '%s/../xsl/tei/tei-xsl-5.9/p5/xhtml/tei.xsl' % path
+
+# Our Java executable
+JAVA = '/usr/bin/java'
+
+# epubcheck location
+EPUBCHECK = '%s/../test/epubcheck.jar' % path
+
+
+# _______________________________________________________________________
 # You should not have to change any items below this as they are standard
 # OPF filenames
 
@@ -58,3 +79,4 @@ CONTAINER_CONTENTS = '''<?xml version="1.0"?>
 
 # TEI namespace
 TEI = 'http://www.tei-c.org/ns/1.0'
+
