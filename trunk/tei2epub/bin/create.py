3d76fda109b42bd60028f2571a7c3542df901280
liza31337 <liza31337@9db8dcc5-964d-0410-8538-7deeb381aa69>
2008-05-19 23:16:47 +0000

    Added xslt

--- trunk/tei2epub/bin/create.py
+++ trunk/tei2epub/bin/create.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
+import os, os.path, sys, logging, shutil, subprocess
 from lxml import etree
-import os, os.path, sys, logging, shutil
 
-from settings import *
-
-logging.basicConfig(level=logging.INFO)
+import settings
 
 def create_html(directory, tree):
     '''Generate the HTML files that make up each chapter in the TEI document.'''
-    xslt = etree.parse(TEI2XHTML_XSLT)
+    xslt = etree.parse(settings.TEI2XHTML_XSLT)
 
     transform = etree.XSLT(xslt)
-    for (i, element) in enumerate(tree.xpath('//tei:div[@type="chapter"]', namespaces={'tei': TEI})):
-        processed = transform(element)
-        f = '%s/%s/chapter-%d.html' % (directory, OEBPS, i + 1)
+    for (i, element) in enumerate(tree.xpath('//tei:div[@type="%s"]' % settings.TEI_DIV_TYPE, namespaces={'tei': settings.TEI})):
+        processed = transform(element, xhtml="'true'", generateParagraphIDs="'true'")
+        f = '%s/%s/chapter-%d.html' % (directory, settings.OEBPS, i + 1)
         _output_html(f, processed) 
 
     # Create the title page
-    _output_html('%s/%s/title_page.html' % (directory, OEBPS), '<p>Title page</p>', False)
+    _output_html('%s/%s/title_page.html' % (directory, settings.OEBPS), '<p>Title page</p>', False)
 
 def _output_html(f, content, xml=True):
     if xml:                     
-        xslt = etree.parse(HTMLFRAG2HTML_XSLT)        
+        xslt = etree.parse(settings.HTMLFRAG2HTML_XSLT)        
         processed = content.xslt(xslt)
         html = etree.tostring(processed, encoding='utf-8', pretty_print=True, xml_declaration=False)                    
     else:
@@ -41,16 +39,16 @@ def _output_html(f, content, xml=True):
 
 def create_content(directory, tree):
     '''Create the content file based on our TEI source'''
-    xslt = etree.parse(TEI2OPF_XSLT)
+    xslt = etree.parse(settings.TEI2OPF_XSLT)
     processed = tree.xslt(xslt)
-    f = '%s/%s/%s' % (directory, OEBPS, CONTENT)
+    f = '%s/%s/%s' % (directory, settings.OEBPS, settings.CONTENT)
     _output_xml(f, processed)
 
 def create_navmap(directory, tree):
     '''Create the navmap file based on our TEI source'''
-    xslt = etree.parse(TEI2NCX_XSLT)
+    xslt = etree.parse(settings.TEI2NCX_XSLT)
     processed = tree.xslt(xslt)
-    f = '%s/%s/%s' % (directory, OEBPS, NAVMAP)
+    f = '%s/%s/%s' % (directory, settings.OEBPS, settings.NAVMAP)
     _output_xml(f, processed)
 
 def _output_xml(f, xml):
@@ -61,40 +59,53 @@ def _output_xml(f, xml):
 
 def create_mimetype(directory):
     '''Create the mimetype file'''
-    f = '%s/%s' % (directory, MIMETYPE)
+    f = '%s/%s' % (directory, settings.MIMETYPE)
     logging.debug('Creating mimetype file %s' % f)
     f = open(f, 'w')
-    f.write(MIMETYPE_CONTENT)
+    f.write(settings.MIMETYPE_CONTENT)
     f.close()
 
 def create_folders(directory):
     '''Create all the top-level directories in our package'''
-    for f in FOLDERS:
+    for f in settings.FOLDERS:
         d = '%s/%s' % (directory, f)
         if not os.path.exists(d):
             os.mkdir(d)
 
 def create_container(directory):
     '''Create the OPF container file'''
-    f = '%s/%s/%s' % (directory, META, CONTAINER)
+    f = '%s/%s/%s' % (directory, settings.META, settings.CONTAINER)
     logging.debug('Creating container file %s' % f)
     f = open(f, 'w')
-    f.write(CONTAINER_CONTENTS)
+    f.write(settings.CONTAINER_CONTENTS)
     f.close()
 
 def create_stylesheet(directory):
     '''Create the stylesheet file'''
-    f = '%s/%s/%s' % (directory, OEBPS, CSS_STYLESHEET)
+    f = '%s/%s/%s' % (directory, settings.OEBPS, settings.CSS_STYLESHEET)
     logging.debug('Creating CSS file %s' % f)
     f = open(f, 'w')
-    f.write(STYLESHEET_CONTENTS)
+    f.write(settings.STYLESHEET_CONTENTS)
     f.close()
 
+def create_package(directory):
+    '''Archive the entire package using our zip utility'''
+    epub_file = '%s.epub' % directory
+    
+    subprocess.check_call([settings.ZIP, settings.ZIP_ARGS, epub_file, settings.MIMETYPE, settings.META, settings.OEBPS], cwd=directory)
+    shutil.move(epub_file, settings.DIST)
 
+def validate(directory):
+    '''Validate this using epubcheck'''
+    output = directory.replace(settings.BUILD, settings.DIST)
+    os.system('%s -jar %s %s.epub' % (settings.JAVA, settings.EPUBCHECK, output))
+        
+                  
 def main(*args):
     '''Create an epub-format zip file given a source XML file.
        Based on the tutorial from: http://www.jedisaber.com/eBooks/tutorial.asp
     '''
+
     if len(args) < 2:
         print 'Usage: create-epub.py tei-source-file [alternate output directory]'
         return 1
@@ -102,20 +113,20 @@ def main(*args):
     source = args[1]
 
     if len(args) > 2:
-        directory = '%s/%s' % (BUILD, args[2])
+        directory = '%s/%s' % (settings.BUILD, args[2])
     else:
         if not '.xml' in source:
             logging.error('Source file must have a .xml extension')
             return 1
-        directory = '%s/%s' % (BUILD, os.path.basename(source).replace('.xml', ''))
+        directory = '%s/%s' % (settings.BUILD, os.path.basename(source).replace('.xml', ''))
 
     tree = etree.parse(source)
 
-    if not os.path.exists(BUILD):
-        os.mkdir(BUILD)
+    if not os.path.exists(settings.BUILD):
+        os.mkdir(settings.BUILD)
 
-    if not os.path.exists(DIST):
-        os.mkdir(DIST)
+    if not os.path.exists(settings.DIST):
+        os.mkdir(settings.DIST)
 
     if os.path.exists(directory):
         logging.debug('Removing previous output directory %s' % directory)
@@ -134,13 +145,16 @@ def main(*args):
     create_html(directory, tree)
 
     # Create the epub format
-    os.chdir(directory)
-    os.system('%s -v0Xq %s %s' % (ZIP, directory, MIMETYPE))
-    os.system('%s -vrq %s * -x %s.zip %s' % (ZIP, directory, directory, MIMETYPE))
-    os.rename('%s.zip' % directory, '%s.epub' % directory)
-    shutil.move('%s.epub' % directory, DIST)
+    create_package(directory)
 
+    # Validate
+    if settings.VALIDATE:
+        return validate(directory)
+
+    logging.warn('Skipping validation step')
     return 0
+
+
 
 if __name__ == '__main__':
     sys.exit(main(*sys.argv))
