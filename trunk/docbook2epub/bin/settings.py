3bbf8ec345ef1d2eaaf3962d688456d99e891ecf
liza31337 <liza31337@9db8dcc5-964d-0410-8538-7deeb381aa69>
2008-11-15 00:09:59 +0000

    Initial add

---
+++ trunk/docbook2epub/bin/settings.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+MIMETYPE = 'mimetype'
+MIMETYPE_CONTENT = 'application/epub+zip'
+DOCBOOK_XSL = '/path/to/docbook/epub/docbook.xsl'
+
+COVER_IS_LINEAR = True
