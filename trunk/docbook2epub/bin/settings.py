8fdef22f5da3bfacd2b30a93c0c7c7540d84bf31
liza31337 <liza31337@9db8dcc5-964d-0410-8538-7deeb381aa69>
2008-11-18 00:46:13 +0000

    Cleaning up

--- trunk/docbook2epub/bin/settings.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-
-MIMETYPE = 'mimetype'
-MIMETYPE_CONTENT = 'application/epub+zip'
-DOCBOOK_XSL = '/path/to/docbook/epub/docbook.xsl'
-
-COVER_IS_LINEAR = True
