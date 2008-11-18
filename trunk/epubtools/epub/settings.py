4eae375dfbb1c777c223bf416cc84751800783f4
liza31337 <liza31337@9db8dcc5-964d-0410-8538-7deeb381aa69>
2008-11-18 00:42:24 +0000

    Updates to new layout

---
+++ trunk/epubtools/epub/settings.py
@@ -0,0 +1,10 @@
+MIMETYPE = 'mimetype'
+MIMETYPE_CONTENT = 'application/epub+zip'
+COVER_IS_LINEAR = True
+
+# Our Java executable
+JAVA = '/usr/bin/java'
+
+# epubcheck location
+#EPUBCHECK = 'path/to/epubcheck.jar'
+EPUBCHECK = '/Users/liza/threepress/data/epub/test/epubcheck-1.0.1.jar'
