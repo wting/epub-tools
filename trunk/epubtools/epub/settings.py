30ae58903031b530e33990894b016bd96270f902
liza31337 <liza31337@9db8dcc5-964d-0410-8538-7deeb381aa69>
2008-12-14 01:37:10 +0000

    Improvements in recursive handling of files

--- trunk/epubtools/epub/settings.py
+++ trunk/epubtools/epub/settings.py
@@ -6,5 +6,4 @@ COVER_IS_LINEAR = True
 JAVA = '/usr/bin/java'
 
 # epubcheck location
-#EPUBCHECK = 'path/to/epubcheck.jar'
-EPUBCHECK = '/Users/liza/threepress/data/epub/test/epubcheck-1.0.1.jar'
+EPUBCHECK = 'path/to/epubcheck.jar'
