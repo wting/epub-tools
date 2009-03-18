d254aa354d5cd28af4e05fff6d891cead9657650
abdelazer <abdelazer@9db8dcc5-964d-0410-8538-7deeb381aa69>
2009-03-18 04:29:01 +0000

    Epubcheck external and a script stub

--- trunk/epubtools/epubtangle/epubtangle/epubcheck.py
+++ trunk/epubtools/epubtangle/epubtangle/epubcheck.py
@@ -13,15 +13,15 @@ import os.path
 log = logging.getLogger(__name__)
 
 class EpubCheck:
-    """A class that manages checking the validity of ePub files."""
+    """A class that manages checking the validity of Epub objects."""
     EPUBCHECK_COMMAND = os.path.abspath(os.path.join(os.path.dirname(__file__), 'externals', 'epubcheck'))
 
-    def is_valid(self, epub_document):
-        """Tests the validity of the provided ePub document."""
-        return self._check_validity(epub_document)
+    def is_valid(self, epub):
+        """Tests the validity of the provided Epub."""
+        return self._check_validity(epub)
 
-    def _check_validity(self, epub_document):
-        checking_cmd = '%s %s' % (self.EPUBCHECK_COMMAND, epub_document)
+    def _check_validity(self, epub):
+        checking_cmd = '%s %s' % (self.EPUBCHECK_COMMAND, epub.filename)
         status, output = commands.getstatusoutput(checking_cmd)
         if status == 0:
             log.info("Epubcheck output: %s" % output)
